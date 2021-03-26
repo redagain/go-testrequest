3a8cab5a5c3699d6247e0f409317a01943f474d8
redagain <makecoderedagain@gmail.com>
2021-03-26 11:38:29 +0300

    Change nopResponseWriter

--- stubs.go
+++ stubs.go
@@ -2,17 +2,13 @@ package testrequest
 
 import "net/http"
 
-type nopResponseWriter struct {
-	header http.Header
-}
+type nopResponseWriter struct{}
 
 //NopResponseWriter returns a http.ResponseWriter with a no-op Header, Write and WriteHeader methods.
 //This is a simple stub for testing.
-func NopResponseWriter() http.ResponseWriter {
-	return &nopResponseWriter{header: http.Header{}}
-}
+func NopResponseWriter() http.ResponseWriter { return &nopResponseWriter{} }
 
-func (w *nopResponseWriter) Header() http.Header { return w.header }
+func (w *nopResponseWriter) Header() http.Header { return http.Header{} }
 
 func (w *nopResponseWriter) Write([]byte) (int, error) { return 0, nil }
 
